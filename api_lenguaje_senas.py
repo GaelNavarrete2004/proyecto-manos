e89c73f67a42b71c249819123771f6447ab3eafb
Gael <jose.navarrete9121@alumnos.udg.mx>
2024-10-29 00:14:41 -0600

    test 3

--- api_lenguaje_senas.py
+++ api_lenguaje_senas.py
@@ -5,8 +5,6 @@ import pickle
 import cv2
 import mediapipe as mp
 import numpy as np
-import pyttsx3
-import threading
 
 # Configuración de la API
 app = FastAPI()
@@ -34,14 +32,6 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
                12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W',
                23: 'X', 24: 'Y', 25: 'Z'}
-
-# Inicializar el motor de texto a voz
-engine = pyttsx3.init()
-engine.setProperty('volume', 1.0)
-
-def speak(character):
-    engine.say(character)
-    engine.runAndWait()
 
 # Estructura de la respuesta de la API
 class SignResponse(BaseModel):
@@ -81,9 +71,6 @@ async def recognize_sign(file: UploadFile = File(...)):
         # Predecir el carácter
         prediction = model.predict([np.asarray(data_aux)])
         predicted_character = labels_dict[int(prediction[0])]
-
-        # Ejecutar texto a voz en un hilo separado
-        threading.Thread(target=speak, args=(predicted_character,)).start()
 
         return {"recognized_text": predicted_character}
     
